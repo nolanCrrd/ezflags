fa63b0211a093ec55c9698958dfd15145d84b9ed
nolanCrrd <contact@nolancrrd.fr>
2026-04-03 11:08:31 +0200

    perf(dependance): remove some unnecessary dependances

--- include/ezflags.h
+++ include/ezflags.h
@@ -1,8 +1,6 @@
 #ifndef EZFLAGS_H
 #define EZFLAGS_H
 
-#include <stdbool.h>
-
 /*========================
  * Defines
  * ======================== */
@@ -14,32 +12,34 @@
 /**
  * @brief Type that store the found result of an option of a command line
  */
-typedef struct s_opt {
+typedef struct s_flag {
   char *name;       /**< Nom du flag (-v -> "v",  --verbose -> "verbose") */
-  bool *found;      /**< Adress where store found status */
+  char *found;      /**< Adress where store found status */
   int params_count; /**< Number of parameter that option can have (-1 if until
                        another param) */
   char **params;    /**< Array where store params of the option */
-} opt_t;
+} flag_t;
 
 /*========================
  * Public functions
  * ======================== */
 
 /**
- * @brief Parse all argv and help you to get all options and thair params
+ * @brief Parse all argv and help you to get all flags and thair params
  *
- * @param options options to parse (argv + 1)
+ * @param flags flags to parse (argv + 1)
  * @param opt_array array that contain all the option define with opt_t type
+ * (need a sentinelle)
  * @param still_argv pointer to a variable that will be allocated to store all
- * argv that are not specified options
- * @return -1 in error case | number of options not found | or 0 in total
+ * argv that are not specified flags
+ * @return -1 in error case | number of flags not found | or 0 in total
  * success
  */
-int ezflags(char **options, opt_t *opt_array, char **still_argv[]);
+int ezflags(char **flags, flag_t *opt_array, char **still_argv[]);
 
 /*========================
  * Private functions
  * ======================== */
+void print_flag(flag_t flag);
 
 #endif // EZFLAGS_H
