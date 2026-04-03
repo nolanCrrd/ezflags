264d4d6c5a73734bfa54ec310f865fd8787b429a
nolanCrrd <contact@nolancrrd.fr>
2026-04-03 15:00:55 +0200

    test(app): Start testing the app

--- tests/ezflags_test.c
+++ tests/ezflags_test.c
@@ -1,3 +1,48 @@
-#include "test.h"
+#include "ezflags.h"
+#include <stdio.h>
+#include <unistd.h>
 
-int main(void) { print_test(); }
+int
+main (void)
+{
+    // print_test ();
+
+    const char *args[]
+        = { "-r", "-R", "1", "hello-world.txt", "c-aca.txt", 0 };
+
+    flag_t flags[] = { (flag_t){
+	                   .required = 0,
+	                   .params_count = 1,
+	                   .name = "R",
+	                   .params = NULL,
+	               },
+	               (flag_t){
+	                   .required = 1,
+	                   .params_count = 0,
+	                   .name = "r",
+	                   .params = NULL,
+	               },
+	               { 0 } };
+
+    char **still_argv = NULL;
+
+    ezflags ((char **)&args, (flag_t *)&flags, &still_argv);
+
+    for (int i = 0; flags[i].name; ++i)
+	{
+	    print_flag (flags[i]);
+	}
+
+    printf ("[");
+    for (int i = 0; still_argv[i]; ++i)
+	{
+	    printf ("%s", still_argv[i]);
+	    if (still_argv[i + 1])
+		{
+		    printf (",");
+		}
+	}
+    printf ("]");
+
+    return (0);
+}
--- tests/print_test.c
+++ tests/print_test.c
@@ -2,11 +2,16 @@
 #include "test.h"
 #include "unistd.h"
 
-void print_flag_test() {
-  char found = 0;
-  flag_t flag = {.name = "r", .found = &found, .params_count = 1, NULL};
+void
+print_flag_test ()
+{
+    flag_t flag = { .name = "r", .found = 0, .params_count = 1, NULL };
 
-  print_flag(flag);
+    print_flag (flag);
 }
 
-void print_test() { print_flag_test(); }
+void
+print_test ()
+{
+    print_flag_test ();
+}
