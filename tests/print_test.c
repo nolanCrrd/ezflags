181b6d821f8940ab18402f6d64e50a62b34b2841
nolanCrrd <contact@nolancrrd.fr>
2026-04-03 16:04:59 +0200

    feat(debug): more debug functions

--- tests/print_test.c
+++ tests/print_test.c
@@ -1,6 +1,6 @@
 #include "ezflags.h"
 #include "test.h"
-#include "unistd.h"
+#include <stdio.h>
 
 void
 print_flag_test ()
@@ -8,6 +8,26 @@ print_flag_test ()
     flag_t flag = { .name = "r", .found = 0, .params_count = 1, NULL };
 
     print_flag (flag);
+}
+
+void
+print_ezflags_result (flag_t flags[], char **still_argv)
+{
+    for (int i = 0; flags[i].name; ++i)
+	{
+	    print_flag (flags[i]);
+	}
+
+    printf ("[");
+    for (int i = 0; still_argv[i]; ++i)
+	{
+	    printf ("%s", still_argv[i]);
+	    if (still_argv[i + 1])
+		{
+		    printf (",");
+		}
+	}
+    printf ("]");
 }
 
 void
