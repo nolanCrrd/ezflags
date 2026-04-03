c6d60495ac0f20830ab82aebdad6347d95eff076
nolanCrrd <contact@nolancrrd.fr>
2026-04-03 11:09:07 +0200

    debug: printf function for flag_t

---
+++ src/debug_print.c
@@ -0,0 +1,22 @@
+#include "ezflags.h"
+#include <stdio.h>
+
+void print_flag(flag_t flag) {
+  printf("{\n \tname: %s\n", flag.name);
+
+  if (flag.found) {
+    printf("\tfound: %i\n", *flag.found);
+  }
+
+  printf("\tparams_count: %i\n", flag.params_count);
+
+  if (flag.params) {
+    printf("\tparams: [\n");
+    for (int i = 0; i < flag.params_count; ++i) {
+      printf("\t\t%s,", flag.params[i]);
+    }
+    printf("\t]\n");
+  }
+
+  printf("}\n");
+}
--- src/ezflags.c
+++ src/ezflags.c
@@ -1,3 +1,1 @@
 #include "ezflags.h"
-
-int ezflags(char **argv, opt_t *opt_array, char ***still_argv) { return (0); }
